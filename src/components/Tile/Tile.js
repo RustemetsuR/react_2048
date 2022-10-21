e2196f49b780832766d2975ee598d43476dfdc60
RustemetsuR <ibraimov.rustem2003@gmail.com>
2022-10-21 18:23:09 +0600

    Added responsive styles for Tile

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -5,7 +5,7 @@ const Tile = props => {
     return (
         <div
             style={props.index === 3 ? { marginRight: 0 } : {}}
-            className={"number-block number-block-" + props.number}>
+            className={props.number !== 0 ? "animated pulse number-block number-block-" + props.number : "number-block number-block-" + props.number}>
             <p className="number">{props.number !== 0 ? props.number : ""}</p>
         </div>
     );
