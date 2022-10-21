f666ddfe4ce56d31bc1b51a25e63adf08c1f8d63
RustemetsuR <ibraimov.rustem2003@gmail.com>
2022-10-21 18:17:59 +0600

    Added gameover modal window

---
+++ src/components/Modal/Modal.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import "./Modal.css";
+
+const Modal = props => {
+
+    return (
+        <div
+            className="modal"
+            style={{
+                opacity: !props.modal ? "0" : "1",
+                transition: "all .2s",
+                visibility: !props.modal ? "hidden" : "visible",
+            }}>
+            <div className="modal__inner">
+                <h3 className="modal__title">
+                    {props.gameResults === "win" ? "You won!" : "Game over!"}
+                </h3>
+
+                <button className="modal__restart" onClick={props.onClick}>
+                    Restart
+            </button>
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
