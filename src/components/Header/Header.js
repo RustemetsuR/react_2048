3091abb63a57b3b35c271c3259672797f8823050
RustemetsuR <ibraimov.rustem2003@gmail.com>
2022-10-23 23:27:28 +0600

    Changed link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,9 @@ const Header = () => {
     return (
         <div className="header">
             <Container>
-                    <h1 className="header__logo"><a href="#">2048</a></h1>
+                <h1 className="header__logo">
+                    <a href="https://rustemetsur.github.io/zeon_project_3/">2048</a>
+                </h1>
             </Container>
         </div>
     );
